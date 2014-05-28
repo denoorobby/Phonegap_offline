c1334ab3dd407f6c102d964185e456f1076b0678
denoorobby <denoorobby@gmail.com>
2014-05-28 16:01:50 +0200

    Removed local build files

--- platforms/android/CordovaLib/ant-gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : C:\BitNami\wampstack-5.4.24-0\apache2\htdocs\offline\platforms\android\CordovaLib\ant-build\AndroidManifest.xml \
--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\BitNami\wampstack-5.4.24-0\apache2\htdocs\offline\platforms\android\ant-build\AndroidManifest.xml : \
-C:\BitNami\wampstack-5.4.24-0\apache2\htdocs\offline\platforms\android\AndroidManifest.xml \
